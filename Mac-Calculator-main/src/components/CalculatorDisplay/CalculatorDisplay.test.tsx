b1b922ef058bbd443246e503af1d3db561ec94cc
Ujjwal Tyagi <Ujjwaltyagi2969@gmail.com>
2024-06-21 17:30:29 +0530

    Delete Mac-Calculator-main directory

--- Mac-Calculator-main/src/components/CalculatorDisplay/CalculatorDisplay.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { CalculatorDisplay } from './CalculatorDisplay';
-
-describe('<CalculatorDisplay />', () => {
-  it('should render', () => {
-    render(<CalculatorDisplay value="111" />);
-    expect(screen.getByText(/111/i)).toBeInTheDocument();
-  });
-});
